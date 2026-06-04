770d583fee781f00681dcbc845fc685e80701950
Khushboo Nimje <158611745+Khushboo1206@users.noreply.github.com>
2026-06-04 13:19:47 +0530

    Add PostgreSQL, Flask API, pagination, env config and project cleanup

--- database/postgres.py
+++ database/postgres.py
@@ -1,13 +1,17 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
 
     conn = psycopg2.connect(
-        host="localhost",
-        database="scraping_db",
-        user="postgres",
-        password="postgres9767",
-        port="5432"
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
     )
 
     return conn
